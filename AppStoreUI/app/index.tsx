3447e86d17098b00b6b6bb5b106f70277977601b
briskgaurav <briskgaurav@gmail.com>
2024-11-28 21:13:43 +0530

    1st

--- AppStoreUI/app/index.tsx
+++ AppStoreUI/app/index.tsx
@@ -1,15 +1,12 @@
-import { View, Text, ScrollView } from "react-native";
-import React from "react";
+import { View, Text } from 'react-native'
+import React from 'react'
 
 const index = () => {
   return (
-    <ScrollView>
-      
-      <View className="w-full h-screen bg-blue-200 flex items-center justify-center">
-        <Text>This is a scroll view example</Text>
-      </View>
-    </ScrollView>
-  );
-};
+    <View>
+      <Text>index</Text>
+    </View>
+  )
+}
 
-export default index;
+export default index
